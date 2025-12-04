bde0b9b67961e3056bb5903d534a5feed627b731
2311303-eng <2311303@iub.edu.bd>
2025-12-04 22:41:33 +0600

    Add files via upload

---
+++ Multinational-Pharmaceutical-Company-master/src/main/java/org/example/multinationalpharmaceuticalcompany/administrator_user1/DashboardController.java
@@ -0,0 +1,7 @@
+package org.example.multinationalpharmaceuticalcompany.administrator_user1;
+
+public class DashboardController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ Multinational-Pharmaceutical-Company-master/src/main/java/org/example/multinationalpharmaceuticalcompany/production_manager_user3/goal4Controller.java
@@ -0,0 +1,7 @@
+package org.example.multinationalpharmaceuticalcompany.production_manager_user3;
+
+public class goal4Controller
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
