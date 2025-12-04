5af81618a4ac0ad8a617d878aa2cacfde77c710a
naf-rai-02 <neeti.nafisa@gmail.com>
2025-12-04 19:31:00 +0600

    commit

--- src/main/java/org/example/multinationalpharmaceuticalcompany/sales_marketing_user5/dashboardController.java
+++ src/main/java/org/example/multinationalpharmaceuticalcompany/sales_marketing_user5/dashboardController.java
@@ -1,7 +1,42 @@
 package org.example.multinationalpharmaceuticalcompany.sales_marketing_user5;
 
+import javafx.event.ActionEvent;
+
 public class dashboardController
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void goal2(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void goal1(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void goal8(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void goal7(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void goal6(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void goal5(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void goal4(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void goal3(ActionEvent actionEvent) {
+    }
+}
