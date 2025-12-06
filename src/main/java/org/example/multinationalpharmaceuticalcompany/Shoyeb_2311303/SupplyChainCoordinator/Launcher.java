6867473ed783c57dd5ebe2626c50fdc780d530af
SHOYEB <2311303@iub.edu.bd>
2025-12-06 14:06:15 +0600

    Add files via upload

---
+++ src/main/java/org/example/multinationalpharmaceuticalcompany/Shoyeb_2311303/SupplyChainCoordinator/Launcher.java
@@ -0,0 +1,11 @@
+package com.example.Shoyeb_2311303;
+
+import javafx.application.Application;
+
+import static javafx.application.Application.*;
+
+public class Launcher {
+    public static void main(String[] args) {
+        zlaunch(HelloApplication.class, args);
+    }
+}
