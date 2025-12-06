c6134cca95235f3c982b7602b7783de2d97f481c
SHOYEB <2311303@iub.edu.bd>
2025-12-06 14:04:32 +0600

    Delete src/main/java/org/example/multinationalpharmaceuticalcompany/Shoyeb_2311303 directory

--- src/main/java/org/example/multinationalpharmaceuticalcompany/Shoyeb_2311303/ClinicalTrialCoordinator/loginviewcontroller.java
+++
@@ -1,7 +0,0 @@
-package com.example.Shoyeb_2311303.ClinicalTrialCoordinator;
-
-public class loginviewcontroller
-{
-    @javafx.fxml.FXML
-    public void initialize() {
-    }}
--- src/main/java/org/example/multinationalpharmaceuticalcompany/Shoyeb_2311303/SupplyChainCoordinator/HelloApplication.java
+++
@@ -1,4 +0,0 @@
-package com.example.Shoyeb_2311303;
-
-public class HelloApplication {
-}
